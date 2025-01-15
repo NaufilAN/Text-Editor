54fe233943f2b98062ac6f0b4812d3035bd7be5c
naufil_ansari_05 <naufilansari05@gmail.com>
2025-01-15 12:01:07 -0500

    Added Save As functionality

--- TextEditor.py
+++ TextEditor.py
@@ -1,4 +1,6 @@
 from tkinter import *
+import tkinter.filedialog
+import tkinter.simpledialog
 
 root = Tk()
 root.title("Naufil's Text Editor")
@@ -6,4 +8,18 @@ root.title("Naufil's Text Editor")
 text = Text(root)
 text.grid()
 
-root.mainloop()
+def saveAs():
+
+    global text
+    t = text.get(0.0, END)
+
+    location = tkinter.filedialog.asksaveasfilename(defaultextension=".txt")
+
+    file = open(location, mode="w+")
+    file.write(t)
+    file.close()
+
+saveAsButton = Button(root, text="Save As", command=saveAs)
+saveAsButton.grid()
+
+root.mainloop() 
